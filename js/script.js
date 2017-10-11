d2a438548501f15aefd1acac1665bbf7874ae34f
rjbertille <rjbertille@gmail.com>
2017-10-11 16:32:12 +0200

    Javascript project update

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//Create a "close" button and append it to each list item
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for(i = 0; i < myNodeList.length; i++) {
+	var button = document.createElement("BUTTON");
+	var txt = document.createTextNode("\u00D7");
+	button.className = "close";
+	button.appendChild(txt);
+	myNodeList[i].appendChild(button);
+}
+//Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for(i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    var div = this.parentElement;  
+    div.style.display = "none";   
+    }
+ }
+
+//Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+	if(ev.target.tagName === 'LI') {
+		ev.target.classList.toggle('checked');
+	}
+
+}, false);
+
+//Create a new list item when clicking on the "Add" button
+function add() {
+   var li = document.createElement("li");
+   var inputValue = document.getElementById("myInput").value;
+   var t = document.createTextNode(inputValue);
+   li.appendChild(t);
+        if(inputValue === '') {
+        	alert("You must write something!");
+        } else {
+        	document.getElementById("listDiv").appendChild(li);
+        }
+
+            document.getElementById("myInput").value = "";
+
+
+    var button = document.createElement("BUTTON");
+    var txt = document.createTextNode("\u00D7");
+    button.className = "close";
+    button.appendChild(txt);
+    li.appendChild(button);
+
+    for(i = 0; i < close.length; i++) {
+    	close[i].onclick = function() {
+    		var div = this.parentElement;
+    		div.style.display = "none";
+    	}
+    }
+
+
+}
+
+
+
+
